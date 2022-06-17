83ef2fe4a3150e4bb4e37348ed4a22f971fd51ed
unknown <kooshasamadi@gmail.com>
2022-06-17 13:21:16 +0430

    stripe completed

--- src/Components/payment-form/payment-form.component.jsx
+++ src/Components/payment-form/payment-form.component.jsx
@@ -1,13 +1,45 @@
 import "./payment-form.styles.scss";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { React, useState } from "react";
 
 function PaymentForm() {
+  const [isPaymentFinished, setIsPaymentFinished] = useState(true);
   const stripe = useStripe();
-  const element = useElements();
+  const elements = useElements();
 
-  const PaymentHandlder = (event) => {
-    if (!stripe || !element) {
+  const PaymentHandlder = async (event) => {
+    event.preventDefault();
+    if (!stripe || !elements) {
       return;
+    }
+    
+    setIsPaymentFinished(false);
+    //deconstruct nested object
+    const {
+      paymentIntent: { client_secret },
+    } = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 100000 }),
+    }).then((res) => res.json());
+
+    // console.log(client_secret);
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: { name: "Koosha" },
+      },
+    });
+    setIsPaymentFinished(true);
+    console.log(paymentResult);
+    if (paymentResult.error) {
+      alert(paymentResult.error.message);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("payment succeeded");
+      }
     }
   };
   return (
@@ -15,8 +47,22 @@ function PaymentForm() {
       <h2>Credit Card Payment:</h2>
       <form onSubmit={PaymentHandlder}>
         <CardElement className="payment-card" />
-        <button type="submit" className="btn btn-outline-info btn-payment">
-          Pay now
+        <button
+          type="submit"
+          className={`${
+            !isPaymentFinished ? "disabled" : ""
+          } btn btn-outline-info btn-primary btn-payment`}
+        >
+          {!isPaymentFinished ? (
+            <span
+              className="spinner-border spinner-border-sm"
+              role="status"
+              aria-hidden="true"
+            ></span>
+          ) : (
+            ""
+          )}
+          Pay
         </button>
       </form>
     </div>
